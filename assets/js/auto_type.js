149d6d77672e3da0bc4875002635392abd6500c6
luian alves <luian.alves@gmail.com>
2022-11-18 16:41:54 -0300

    Configurando responsividade pt15

--- assets/js/auto_type.js
+++ assets/js/auto_type.js
@@ -1,7 +1,7 @@
 var textoT = new Typed(".text-type", {
 
-    strings: ["Seu site ..", "Sua ideia ..", "Seu app .."],
-    typeSpeed: 100,
-    backSpeed: 100,
+    strings: ["Seu site", "Sua ideia", "Seu app"],
+    typeSpeed: 140,
+    backSpeed: 140,
     loop: true
 });
