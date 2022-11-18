a22a28db0f6938068ce3be004f856b41f4a0004f
luian alves <luian.alves@gmail.com>
2022-11-18 13:15:07 -0300

    Adicionando novo arquivo css para resposividade

--- assets/js/auto_type.js
+++ assets/js/auto_type.js
@@ -1,6 +1,6 @@
 var textoT = new Typed(".text-type", {
 
-    strings: ["Seu site ..", "Sua ideia ..", "Seu app"],
+    strings: ["Seu site ..", "Sua ideia ..", "Seu app .."],
     typeSpeed: 100,
     backSpeed: 100,
     loop: true
