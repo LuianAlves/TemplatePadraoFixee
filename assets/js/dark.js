be94e0caa8458ae68e6632b86ea6fcbf879ff3cd
luian alves <luian.alves@gmail.com>
2022-11-24 12:32:04 -0300

    Implementando modo dark pt8 - Ajustando cor da onda

--- assets/js/dark.js
+++ assets/js/dark.js
@@ -1,5 +1,13 @@
 const changeDarkMode = document.querySelector("#dark-mode")
+const changeOnda = document.querySelector('[fill]')
 
+function darkModeOnda() {
+    changeOnda.setAttribute("fill", "#ffffff")
+}
+
+function lightModeOnda() {
+    changeOnda.setAttribute("fill", "#1a1a1a")
+}
 
 function darkMode() {
     document.body.classList.toggle("dark")
@@ -10,6 +18,9 @@ function loadDarkMode() {
 
     if (dMode) {
         darkMode()
+        darkModeOnda()
+    } else {
+        lightModeOnda()
     }
 }
 
@@ -17,10 +28,17 @@ loadDarkMode();
 
 changeDarkMode.addEventListener("change", function() {
     darkMode();
+    darkModeOnda();
 
-    localStorage.removeItem("dark");
+
 
     if (!document.body.classList.contains("dark")) {
         localStorage.setItem("dark", 1);
+        localStorage.setItem("darkOnda", 2);
+        localStorage.removeItem("lightOnda");
+    } else {
+        localStorage.setItem("lightOnda", 3);
+        localStorage.removeItem("dark");
+        localStorage.removeItem("darkOnda");
     }
 })
