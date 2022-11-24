ae1ca7062c5d2b2114bcf99e9d39702c997bc119
luian alves <luian.alves@gmail.com>
2022-11-24 11:31:52 -0300

    Implementando modo dark pt6 - Adicionando ao localStorage

--- assets/js/dark.js
+++ assets/js/dark.js
@@ -1,5 +1,26 @@
 const changeDarkMode = document.querySelector("#dark-mode")
 
-changeDarkMode.addEventListener("change", function() {
+
+function darkMode() {
     document.body.classList.toggle("dark")
+}
+
+function loadDarkMode() {
+    const dMode = localStorage.getItem("dark")
+
+    if (dMode) {
+        darkMode()
+    }
+}
+
+loadDarkMode();
+
+changeDarkMode.addEventListener("change", function() {
+    darkMode();
+
+    localStorage.removeItem("dark");
+
+    if (!document.body.classList.contains("dark")) {
+        localStorage.setItem("dark", 1);
+    }
 })
