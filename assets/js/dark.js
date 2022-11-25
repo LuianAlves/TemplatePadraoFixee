5fa6489345331db9629ed5d9dd05c1ed6ed22ca4
luian alves <luian.alves@gmail.com>
2022-11-25 10:50:57 -0300

    Implementando modo dark pt9 - finalizado

--- assets/js/dark.js
+++ assets/js/dark.js
@@ -1,13 +1,4 @@
 const changeDarkMode = document.querySelector("#dark-mode")
-const changeOnda = document.querySelector('[fill]')
-
-function darkModeOnda() {
-    changeOnda.setAttribute("fill", "#ffffff")
-}
-
-function lightModeOnda() {
-    changeOnda.setAttribute("fill", "#1a1a1a")
-}
 
 function darkMode() {
     document.body.classList.toggle("dark")
@@ -18,9 +9,6 @@ function loadDarkMode() {
 
     if (dMode) {
         darkMode()
-        darkModeOnda()
-    } else {
-        lightModeOnda()
     }
 }
 
@@ -28,17 +16,10 @@ loadDarkMode();
 
 changeDarkMode.addEventListener("change", function() {
     darkMode();
-    darkModeOnda();
 
-
-
-    if (!document.body.classList.contains("dark")) {
+    if (document.body.classList.contains("dark")) {
         localStorage.setItem("dark", 1);
-        localStorage.setItem("darkOnda", 2);
-        localStorage.removeItem("lightOnda");
     } else {
-        localStorage.setItem("lightOnda", 3);
         localStorage.removeItem("dark");
-        localStorage.removeItem("darkOnda");
     }
 })
