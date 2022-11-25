513cda89e21b9fda775996b2db1c31468a1b9982
luian alves <luian.alves@gmail.com>
2022-11-25 16:53:48 -0300

    ajustando responsividade pt6

--- assets/js/banner.js
+++ assets/js/banner.js
@@ -7,15 +7,15 @@ $(".carousel").owlCarousel({
 
     responsive: {
         0: {
-            items: 2,
-            nav: false
-        },
-        600: {
             items: 3,
             nav: false
         },
-        1000: {
+        600: {
             items: 4,
+            nav: false
+        },
+        991: {
+            items: 5,
             nav: false
         },
     },
