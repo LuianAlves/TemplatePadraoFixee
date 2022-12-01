a563c82790d079d9cb34228aea2428d823dffebe
luian alves <luian.alves@gmail.com>
2022-12-01 11:51:04 -0300

    botão lado direito fixado

--- assets/js/menu_toggle.js
+++
@@ -1,6 +0,0 @@
-const navigation = document.querySelector('.navigation');
-
-document.querySelector('.toggle-men').onclick = function() {
-    this.classList.toggle('active')
-    navigation.classList.toggle('active')
-}
