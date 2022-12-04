8556934ca7f47489f3b5bc823e5d930009f9b9a8
luian alves <luian.alves@gmail.com>
2022-12-03 23:17:26 -0300

    ajustando height das sections pt3

---
+++ assets/js/carousel_portfolio.js
@@ -0,0 +1,21 @@
+$(".portfolio-slider").owlCarousel({
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+
+    responsive: {
+        0: {
+            items: 1,
+            nav: false
+        },
+        600: {
+            items: 2,
+            nav: false
+        },
+        991: {
+            items: 3,
+            nav: false
+        },
+    },
+});
