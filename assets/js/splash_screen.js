2523d979b01e82f4205688072e6e57f167f8d461
Luian Alves <luian.alves@gmail.com>
2022-12-22 11:11:01 -0300

    adicionando splash screen pt3

--- assets/js/splash_screen.js
+++ assets/js/splash_screen.js
@@ -4,9 +4,10 @@ setTimeout(() => {
     document.getElementById("splash").classList.toggle('fade')
     document.getElementById("home").classList.toggle('fade')
     document.getElementById("btnWhatsapp").classList.toggle('fade')
+    document.getElementById("backToTop").classList.toggle('fade')
     document.getElementsByClassName("scrollbar-c")[0].classList.toggle('body-over')
 }, 15000);
 
 setTimeout(() => {
     document.getElementById("img-splash").classList.toggle('fade')
-}, 5000);
+}, 4000);
