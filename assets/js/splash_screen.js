0a7d9e8ac4804cd2b3ec49d1697ae246e8fa7334
Luian Alves <luian.alves@gmail.com>
2022-12-23 09:58:14 -0300

    adicionando video youtube

--- assets/js/splash_screen.js
+++ assets/js/splash_screen.js
@@ -2,12 +2,10 @@
 setTimeout(() => {
     document.getElementById("section-splash").classList.toggle('fade')
     document.getElementById("splash").classList.toggle('fade')
+    document.getElementById("main").classList.toggle('fade')
     document.getElementById("home").classList.toggle('fade')
-    document.getElementById("btnWhatsapp").classList.toggle('fade')
-    document.getElementById("toggle-men").classList.toggle('fade')
-    document.getElementsByClassName("scrollbar-c")[0].classList.toggle('body-over')
-}, 12000);
+}, 15000);
 
 setTimeout(() => {
     document.getElementById("img-splash").classList.toggle('fade')
-}, 4000);
+}, 5500);
