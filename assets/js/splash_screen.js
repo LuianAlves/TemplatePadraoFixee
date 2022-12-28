c5535e54966b96fbfb2bdc6a224e69bf814132e9
Luian Alves <luian.alves@gmail.com>
2022-12-28 14:42:42 -0300

    Ajustando cor do text na splash screen pt2

--- assets/js/splash_screen.js
+++ assets/js/splash_screen.js
@@ -4,4 +4,4 @@ setTimeout(() => {
     document.getElementById("splash").classList.toggle('fade')
     document.getElementById("main").classList.toggle('fade')
     document.getElementById("home").classList.toggle('fade')
-}, 8000);
+}, 8000000);
