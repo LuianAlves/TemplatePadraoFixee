0562c788d7cda689f38aa41ac2d615b8640334a7
Luian Alves <luian.alves@gmail.com>
2023-01-12 12:35:47 -0300

    adicionando site thiago

--- public/assets/js/splash_screen.js
+++ public/assets/js/splash_screen.js
@@ -3,5 +3,6 @@ setTimeout(() => {
     document.getElementById("section-splash").classList.toggle('fade')
     document.getElementById("splash").classList.toggle('fade')
     document.getElementById("main").classList.toggle('fade')
+    document.getElementById("btnWhatsapp").classList.toggle('fade')
     document.getElementById("home").classList.toggle('fade')
 }, 7500);
